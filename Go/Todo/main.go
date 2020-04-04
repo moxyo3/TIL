a3c705ccb98f688a3d7d01ed7332d55f563ed1f2
moxyo3 <1254momomo@gmail.com>
2020-04-04 13:04:23 +0000

    todo_memo

--- Go/Todo/main.go
+++ Go/Todo/main.go
@@ -5,9 +5,10 @@ import(
 )
 
 func main(){
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/todos", fun(w http.ResponseWriter, r *http.Request))
   type Todo struct{
    ID int
    name string
-
   }
 }
