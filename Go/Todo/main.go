6f1c9eec4b22adeaaec77acc1d6b66604e74342e
moxyo3 <60541036+moxyo3@users.noreply.github.com>
2020-04-05 11:03:00 +0900

    Update main.go

--- Go/Todo/main.go
+++ Go/Todo/main.go
@@ -1,14 +1,27 @@
 package main
 
-import(
-  "net/http"
+import (
+	"encoding/json"
+	"net/http"
 )
 
-func main(){
+func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.HandleFunc("/todos", fun(w http.ResponseWriter, r *http.Request))
-  type Todo struct{
-   ID int
-   name string
+	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request){
+	createTodo(w,r)
+	})
+
+  type Todo struct {
+   ID int		`json:"id"`
+   Name string	`json:"name"`
+   Todo string	`json:"todo"`
   }
+}
+
+func createTodo(w ResponseWriter, r *http.Request) {
+	var todo Todo
+	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil{
+		http.Error(w, err.Error(), 500)
+		return
+	}
 }
