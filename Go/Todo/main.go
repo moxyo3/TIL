82615295ead379ca7d6c38362e3be917294645b4
moxyo3 <1254momomo@gmail.com>
2020-04-07 16:29:16 +0000

    js

--- Go/Todo/main.go
+++ Go/Todo/main.go
@@ -94,7 +94,7 @@ func getTodo(w http.ResponseWriter, r *http.Request){
 
 func deleteTodo(w http.ResponseWriter, r *http.Request){
 	id := r.URL.Query().Get("id");
-	if (id == ""){
+	if id == "" {
 		http.Error(w,"id parameter is not found", 500)
 		return
 	}
